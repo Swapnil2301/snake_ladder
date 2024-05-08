dd2c8c4a8accda8a1635eb7ed0d05bc921e6c628
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-08 11:44:15 +0530

    file added

---
+++ Snake_ladder.java
@@ -0,0 +1,10 @@
+
+public class Snake_ladder {
+
+    public static void main(String[] args) {
+        int Win_condition =100;
+        int player_position =1;
+        System.out.println("Welcome to Game");
+        System.out.println("player Postion is"+ player_position);
+    }
+}
